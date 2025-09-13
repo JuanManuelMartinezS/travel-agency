d1b0eda049c4016a9d2ee3f76aa9677269f7b508
JuanManuelMartinez <juanmanuel.martinez@softory.co>
2025-09-12 20:43:17 -0500

    Reusing code

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  url_ms_cinema:"https://8147658a-7b33-44a0-b4de-e1de5ace3b0a.mock.pstmn.io",
+  url_ms_security:"https://c18288b0-cb40-471b-b43d-aa127fec56ba.mock.pstmn.io",
+  url_ms_socket:"http://localhost:5000",
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
