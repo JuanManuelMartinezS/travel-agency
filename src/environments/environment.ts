622f2822c9e1f2d4fc14293f1028a1c194d7defa
JuanManuelMartinez <juanmanuel.martinez@softory.co>
2025-09-12 21:06:08 -0500

    Plantilla nueva

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,9 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  url_ms_cinema:"https://8147658a-7b33-44a0-b4de-e1de5ace3b0a.mock.pstmn.io",
-  url_ms_security:"https://c18288b0-cb40-471b-b43d-aa127fec56ba.mock.pstmn.io",
-  url_ms_socket:"http://localhost:5000",
 };
 
 /*
@@ -16,4 +13,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
